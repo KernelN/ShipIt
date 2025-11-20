0d7683f19132a653d2df2b042553251c90110168
Alejandro Insausti <alejandroninsausti@gmail.com>
2025-11-20 14:14:15 -0300

    Add fuel system

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,7 @@ namespace ShipIt
 {
     public class GameManager : Universal.Singleton<GameManager>
     {
+        internal override bool DoNotDestroyOnLoad => true;
         const string DataPath = "/Data/GameData.dat";
 
         GameData data;
@@ -14,17 +15,12 @@ namespace ShipIt
         {
             base.Awake();
 
-            if (this != inst)
-            {
-                return;
-            }
+            if (this != inst) return;
 
             LoadGameData();
 
-            if (data == null)
-            {
+            if (data == null) 
                 data = new GameData();
-            }
         }
 
         //Methods
@@ -33,17 +29,21 @@ namespace ShipIt
             SaveGameData();
             Application.Quit();
         }
-
         public void SaveGameData()
         {
             string path = Application.persistentDataPath + DataPath;
             FileManager<GameData>.SaveDataToFile(data, path);
         }
-
         public void LoadGameData()
         {
             string path = Application.persistentDataPath + DataPath;
             data = FileManager<GameData>.LoadDataFromFile(path);
+        }
+        [ContextMenu("Clear Game Data")]
+        public void ClearGameData()
+        {
+            data = new GameData();
+            SaveGameData();
         }
     }
 }
